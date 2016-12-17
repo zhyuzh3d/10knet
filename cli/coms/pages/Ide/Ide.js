0ade966461059d5c1d0f0cdbb030deb6eaaf10e3
zhyuzh <zhyuzh@zhyuzhs-MacBook-Air.local>
2016-12-17 21:33:24 +0800

    测试全本地化的editor编辑器雏形

--- cli/coms/pages/Ide/Ide.js
+++ cli/coms/pages/Ide/Ide.js
@@ -5,10 +5,18 @@ export default com;
 let vc; //此元素vueComponent对象
 let jo; //此元素对应的jquery对象,mounted函数内设定
 
+//所有要用的元素都写在这里
+import Editor from '../../blocks/Editor/Editor.html';
+com.components = {
+    Editor
+};
+
+
+
 com.data = function data() {
     vc = this;
     return {
-        msg: 'Hello from blocks/Ide/Ide.js'
+        msg: 'Hello from blocks/Ide/Ide.jsX'
     };
 };
 
@@ -17,13 +25,14 @@ com.mounted = function () {
 
     //激活顶部导航栏菜单
     vc.$xrouter.xset('NavBar', {
-    activeMenu: 'ide',
-});
+        activeMenu: 'ide',
+    });
 
     //使用导航栏背景
     vc.$xrouter.xset('App', {
         barBg: '',
     });
+
 };
 
 //-------所有函数写在下面,可以直接使用vc，jo；禁止在下面直接运行--------
---
+++ pub/templates/preview/preview.js
@@ -0,0 +1,110 @@
+//刷新iframe页面
+function reload() {
+    location.reload();
+};
+
+//载入后自动从保存的ls中恢复
+function autoRefresh() {
+    refreshCss();
+    refreshHtml();
+    reloadJs();
+};
+
+autoRefresh();
+
+function refresh(params) {
+    if (!params) return;
+
+    var lang = params.lang;
+
+    switch (lang) {
+        case 'css':
+            refreshCss();
+            break;
+        case 'html':
+            refreshHtml();
+            break;
+        case 'js':
+            refreshJs();
+            break;
+        default:
+            console.log('refresh:lang is unknown', lang);
+            break;
+    };
+};
+
+function refreshCss() {
+    var data = localStorage.getItem('preview-css');
+    $('style[preview]').empty();
+    $('style[preview]').append(data);
+};
+
+function refreshHtml() {
+    var data = localStorage.getItem('preview-html');
+    $('body').empty();
+    $('body').append(data);
+};
+
+function refreshJs() {
+    //reload();
+};
+
+function reloadJs() {
+    var data = localStorage.getItem('preview-js');
+    $('script[preview]').remove();
+    var sdom = $('<script preview></script>');
+    sdom.html(data);
+    $('body').append(sdom);
+};
+
+
+
+/**
+ * 接收父层窗口传来的信息,字符串化的对象;
+ * {id:'...',cmd:'refresh',params:{lang:'css'}};
+ */
+window.addEventListener('message', function (e) {
+    //禁止跨域访问
+    if (e.source.location.host != location.host) {
+        console.log('preview/message:source illegal!');
+        return;
+    };
+
+    var obj = JSON.safeParse(e.data);
+
+    if (obj) {
+        //执行刷新
+        if (obj.cmd == 'refresh') {
+
+            refresh(obj.params);
+        } else if (obj.cmd == 'reload') {
+            reload();
+        }
+    } else {
+        console.warn('preview/message:data format err', e.data);
+    };
+
+    //发送回执
+    var ackmsg = { //回执对象
+        cmd: 'ACK',
+        time: new Date().getTime(),
+        id: obj ? obj.id : undefined,
+    };
+    e.ports[0].postMessage('ACK');
+}, false);
+
+
+
+
+/**
+ * 扩展JSON安全parse方法
+ * @param   {string} str 字符串
+ * @returns {object} 成功的对象或undefined
+ */
+JSON.safeParse = function (str) {
+    try {
+        return JSON.parse(str);
+    } catch (err) {
+        return undefined;
+    };
+};
